91b86203c8ae1139e1a366f31917cc45bf3beefc
lindenrty <lindenrty@hotmail.com>
2020-04-27 19:52:15 +0800

    无法发布独立程序的问题的修改(移除XSS过滤处理)

--- src/YL.Utils/Security/Xss.cs
+++ src/YL.Utils/Security/Xss.cs
@@ -1,18 +1,18 @@
-﻿using Ganss.XSS;
+﻿//using Ganss.XSS;
 
 namespace YL.Utils.Security
 {
     public class Xss
     {
-        private HtmlSanitizer htmlSanitizer;
+        //private HtmlSanitizer htmlSanitizer;
         private static Xss _xss;
 
         public Xss()
         {
-            htmlSanitizer = new HtmlSanitizer();
-            //htmlSanitizer.AllowedTags.Add("div");//标签白名单
-            htmlSanitizer.AllowedAttributes.Add("class");//标签属性白名单,默认没有class标签属性
-            //htmlSanitizer.AllowedCssProperties.Add("font-family");//CSS属性白名单
+            //htmlSanitizer = new HtmlSanitizer();
+            ////htmlSanitizer.AllowedTags.Add("div");//标签白名单
+            //htmlSanitizer.AllowedAttributes.Add("class");//标签属性白名单,默认没有class标签属性
+            ////htmlSanitizer.AllowedCssProperties.Add("font-family");//CSS属性白名单
         }
 
         public static Xss GetXss()
@@ -27,8 +27,8 @@ namespace YL.Utils.Security
         /// <returns>过滤结果</returns>
         public string Filter(string html)
         {
-            var str = htmlSanitizer.Sanitize(html);
-            return str;
+            //var str = htmlSanitizer.Sanitize(html);
+            return html;
         }
     }
 }
