a3143b86ed2dfb4b90580774b005b593ceb56359
lindenrty <lindenrty@hotmail.com>
2020-07-03 23:31:56 +0800

    增加立库使用情况画面

---
+++ src/WmsCore/Controllers/StatController.cs
@@ -0,0 +1,42 @@
+﻿using IServices;
+using Microsoft.AspNetCore.Mvc;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WMSCore.Outside;
+using YL.Core.Dto;
+using YL.Core.Entity;
+using YL.Core.Entity.Fluent.Validation;
+using YL.NetCore.Attributes;
+using YL.NetCore.NetCoreApp;
+using YL.Utils.Extensions;
+using YL.Utils.Pub;
+using YL.Utils.Table;
+using YL.Utils.Json;
+using static YL.Core.Dto.PubParams;
+using Newtonsoft.Json;
+using IServices.Outside;
+
+namespace KopSoftWms.Controllers
+{
+    public class StatController : BaseController
+    { 
+
+        private readonly SqlSugarClient _client; 
+        public StatController( 
+            SqlSugarClient client
+            )
+        {
+            _client = client; 
+        }
+
+        [HttpGet] 
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+         
+    }
+}
